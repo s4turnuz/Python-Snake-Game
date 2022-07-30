293d9791b78d8c584a0c87ac84b44edce0472460
Douglas Sammur <douglassammur@alunos.utfpr.edu.br>
2022-07-30 11:46:01 -0300

    att. dynamic interaction

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-import random
-import pygame
+import pygame, random
 from pygame.locals import *
 
+def on_grid_random():
+    x = random.randint(0,590)
+    y = random.randint(0,590)
+    return (x//10 * 10, y//10 * 10)
+
+def catch(snake, apple):
+    return (snake[0] == apple[0]) and (snake[1] == apple[1])
+
+# definitions
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -11,25 +19,57 @@ pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake')
 
+# Snake set
 snake = [(200,200), (210,200), (220,200)]
 snake_skin = pygame.Surface((10,10))
-snake_skin.fill((255,255,255)) # white color
+snake_skin.fill((255,255,255))
 
+# Apple set
+apple_pos = on_grid_random()
 apple = pygame.Surface((10,10))
-apple.fill((255,0,0)) # red color
-apple_pos = (random.randint(0,590), random.randint(0,590))
+apple.fill((255,0,0))
 
+# start direction
 my_direction = LEFT
 
+clock = pygame.time.Clock()
+
 while True:
-    for event in pygame.event.get(): # exit to game
+    clock.tick(10)
+    for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-    
-    screen.fill((0,0,0)) # clear the screen
-    screen.blit(apple, apple_pos)
 
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_direction = UP
+            if event.key == K_DOWN:
+                my_direction = DOWN
+            if event.key == K_LEFT:
+                my_direction = LEFT
+            if event.key == K_RIGHT:
+                my_direction = RIGHT
+
+    if catch(snake[0], apple_pos):
+        apple_pos = on_grid_random()
+        snake.append((0,0))
+
+    for i in range(len(snake) - 1, 0, -1): # add position to new tail
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+
+    if my_direction == UP:
+        snake[0] = (snake[0][0], snake[0][1] - 10)
+    if my_direction == DOWN:
+        snake[0] = (snake[0][0], snake[0][1] + 10)
+    if my_direction == RIGHT:
+        snake[0] = (snake[0][0] + 10, snake[0][1])
+    if my_direction == LEFT:
+        snake[0] = (snake[0][0] - 10, snake[0][1])
+
+    screen.fill((0,0,0))
+    screen.blit(apple, apple_pos)
+    
     for pos in snake:
-        screen.blit(snake_skin, pos)
+        screen.blit(snake_skin,pos)
 
     pygame.display.update()
